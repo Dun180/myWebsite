787f4d8237ba5c11bcf6c76b97e4f49a04796d00
Dun180 <74287755+Dun180@users.noreply.github.com>
2021-05-12 10:43:54 +0800

    start

---
+++ src/main/java/com/dun/config/MyMvcConfig.java
@@ -0,0 +1,16 @@
+package com.dun.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/backstage.html").setViewName("backstage");
+    }
+}
